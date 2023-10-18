227ab223131fe2adc47e7f40b3d95ca4b2f0c1a3
TroyHeizer <133907479+TroyHeizer@users.noreply.github.com>
2023-10-18 16:12:24 -0400

    Update KC3.sql
    changed format

--- KC3.sql
+++ KC3.sql
@@ -1,11 +1,10 @@
-//"Select the Customer Name for the OrderID 10310"//
+--Select the Customer Name for the OrderID 10310
 SELECT C.CustomerName
 FROM Customers AS C
 INNER JOIN Orders AS O ON C.CustomerID = O.CustomerID
 WHERE O.OrderID = 10310;
 
-//"Select the address for the supplier of ProductID 40"//
-
+--Select the address for the supplier of ProductID 40
 SELECT S.Address
 FROM Suppliers AS S
 INNER JOIN Products AS P ON S.SupplierID = P.SupplierID
